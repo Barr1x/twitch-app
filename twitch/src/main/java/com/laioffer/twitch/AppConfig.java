37b0a121c032b9f317830ba4703e86ddfe95298f
Barr1x <boningxing743@gmail.com>
2025-06-23 19:00:05 +0800

    complete User register

---
+++ twitch/src/main/java/com/laioffer/twitch/AppConfig.java
@@ -0,0 +1,44 @@
+package com.laioffer.twitch;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/**").permitAll()
+                );
+        return http.build();
+    }
+
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
+
+
