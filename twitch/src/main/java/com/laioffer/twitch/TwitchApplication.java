8fb7232c7bb404e5593ab62a84ca55cbe02b364d
Barr1x <boningxing743@gmail.com>
2025-06-25 16:27:19 +0800

    add unit test and caching

--- twitch/src/main/java/com/laioffer/twitch/TwitchApplication.java
+++ twitch/src/main/java/com/laioffer/twitch/TwitchApplication.java
@@ -1,15 +1,22 @@
 package com.laioffer.twitch;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.laioffer.twitch")
+@EnableFeignClients
+@EnableCaching
 public class TwitchApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(TwitchApplication.class, args);
     }
 
+
 }
+
--- twitch/src/main/java/com/laioffer/twitch/favorite/FavoriteService.java
+++ twitch/src/main/java/com/laioffer/twitch/favorite/FavoriteService.java
@@ -7,6 +7,7 @@ import com.laioffer.twitch.db.entity.FavoriteRecordEntity;
 import com.laioffer.twitch.db.entity.ItemEntity;
 import com.laioffer.twitch.db.entity.UserEntity;
 import com.laioffer.twitch.model.TypeGroupedItemList;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,7 +30,7 @@ public class FavoriteService {
         this.favoriteRecordRepository = favoriteRecordRepository;
     }
 
-
+    @CacheEvict(cacheNames = "recommend_items", key = "#user")
     @Transactional
     public void setFavoriteItem(UserEntity user, ItemEntity item) {
         ItemEntity persistedItem = itemRepository.findByTwitchId(item.twitchId());
@@ -43,7 +44,7 @@ public class FavoriteService {
         favoriteRecordRepository.save(favoriteRecord);
     }
 
-
+    @CacheEvict(cacheNames = "recommend_items", key = "#user")
     public void unsetFavoriteItem(UserEntity user, String twitchId) {
         ItemEntity item = itemRepository.findByTwitchId(twitchId);
         if (item != null) {
