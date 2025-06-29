42f76d8d1115be3884928765e150add6de5eda13
Barr1x <boningxing743@gmail.com>
2025-06-29 21:35:07 +0800

    finish frontend code

--- twitchfe/src/App.js
+++ twitchfe/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, message } from 'antd';
-import { logout, checkUser} from './utils';
+import { Layout, message, Menu } from 'antd';
+import { LikeOutlined, FireOutlined } from '@ant-design/icons';
+import { logout, getFavoriteItem, getTopGames, searchGameById, getRecommendations, checkUser } from './utils';
 import PageHeader from './components/PageHeader';
+import CustomSearch from './components/CustomSearch';
+import Home from './components/Home';
 
 
 const { Header, Content, Sider } = Layout;
@@ -9,6 +12,13 @@ const { Header, Content, Sider } = Layout;
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
+  const [favoriteItems, setFavoriteItems] = useState([]);
+  const [topGames, setTopGames] = useState([])
+  const [resources, setResources] = useState({
+    videos: [],
+    streams: [],
+    clips: [],
+  });
 
   useEffect(
     () => {
@@ -18,8 +28,22 @@ function App() {
     }
   , [])
 
+
+  useEffect(() => {
+    getTopGames()
+      .then((data) => {
+        setTopGames(data)
+      }).catch((err) => {
+        message.error(err.message)
+      })
+  }, [])
+
+
   const signinOnSuccess = () => {
     setLoggedIn(true);
+    getFavoriteItem().then((data) => {
+      setFavoriteItems(data);
+    });
   }
 
 
@@ -33,16 +57,82 @@ function App() {
   }
 
 
+  const customSearchOnSuccess = (data) => {
+    setResources(data);
+  }
+
+
+  const onGameSelect = ({ key }) => {
+    if (key === "recommendation") {
+      getRecommendations().then((data) => {
+        setResources(data);
+      });
+      return;
+    }
+
+
+    searchGameById(key).then((data) => {
+      setResources(data);
+    });
+  };
+
+    const favoriteOnChange = () => {
+    getFavoriteItem()
+      .then((data) => {
+        setFavoriteItems(data);
+      })
+      .catch((err) => {
+        message.error(err.message);
+      });
+  };
+
+
+
+  const mapTopGamesToProps = (topGames) => [
+    {
+      label: "Recommend for you!",
+      key: "recommendation",
+      icon: <LikeOutlined />,
+    },
+    {
+      label: "Popular Games",
+      key: "popular_games",
+      icon: <FireOutlined />,
+      children: topGames.map((game) => ({
+        label: game.name,
+        key: game.id,
+        icon:
+          <img
+            alt="placeholder"
+            src={game.box_art_url.replace('{height}', '40').replace('{width}', '40')}
+            style={{ borderRadius: '50%', marginRight: '20px' }}
+          />
+      }))
+    }
+  ]
+
+
+
+
   return (
     <Layout>
+      <Header>
         <PageHeader
           loggedIn={loggedIn}
           signoutOnClick={signoutOnClick}
           signinOnSuccess={signinOnSuccess}
+          favoriteItems={favoriteItems}
         />
+      </Header>
       <Layout>
         <Sider width={300} className="site-layout-background">
-          {'Sider'}
+          <CustomSearch onSuccess={customSearchOnSuccess} />
+          <Menu
+            mode="inline"                   
+            onSelect={onGameSelect}
+            style={{ marginTop: '10px' }}
+            items={mapTopGamesToProps(topGames)}
+          />
         </Sider>
         <Layout style={{ padding: '24px' }}>
           <Content
@@ -54,7 +144,12 @@ function App() {
               overflow: 'auto'
             }}
           >
-            {'Home'}
+            <Home
+              resources={resources}
+              loggedIn={loggedIn}
+              favoriteOnChange={favoriteOnChange}
+              favoriteItems={favoriteItems}
+            />
           </Content>
         </Layout>
       </Layout>
--- twitchfe/src/components/PageHeader.js
+++ twitchfe/src/components/PageHeader.js
@@ -1,4 +1,5 @@
 import { Layout, Row, Col, Button } from 'antd'
+import Favorites from './Favorites'
 import Register from './Register'
 import Login from './Login'
 import React from 'react'
@@ -7,12 +8,12 @@ import React from 'react'
 const { Header } = Layout
 
 
-function PageHeader({ loggedIn, signoutOnClick, signinOnSuccess }) {
+function PageHeader({ loggedIn, signoutOnClick, signinOnSuccess, favoriteItems }) {
   return (
     <Header>
-      <Row justify="space-between">
+      <Row justify='space-between'>
         <Col>
-          {'Favorites'}
+          {loggedIn && <Favorites favoriteItems={favoriteItems} />}
         </Col>
         <Col>
           {loggedIn && <Button shape="round" onClick={signoutOnClick}>Logout</Button>}
